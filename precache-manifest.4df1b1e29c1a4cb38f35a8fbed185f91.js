c7ffc6c43f3d63e97e70c626e7413a40b94f7716
Mounira94 <mounira.sghari94@gmail.com>
2018-12-04 17:05:46 +0100

    Updates

---
+++ precache-manifest.4df1b1e29c1a4cb38f35a8fbed185f91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "41479b61c5962beb1efc",
+    "url": "/movie-app-redux/static/js/runtime~main.41479b61.js"
+  },
+  {
+    "revision": "03f97837f614ab2ce050",
+    "url": "/movie-app-redux/static/js/main.03f97837.chunk.js"
+  },
+  {
+    "revision": "f5ccdedee7a952a6d484",
+    "url": "/movie-app-redux/static/js/1.f5ccdede.chunk.js"
+  },
+  {
+    "revision": "03f97837f614ab2ce050",
+    "url": "/movie-app-redux/static/css/main.a370f2a1.chunk.css"
+  },
+  {
+    "revision": "611195a5a8707081a76c07ecab7ae6c9",
+    "url": "/movie-app-redux/index.html"
+  }
+];
